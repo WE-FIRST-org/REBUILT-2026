db62fb75ac035e8d6b5ce0c7c6fdc8dfc4b48148
Emily Wu <emily.wu918@gmail.com>
2026-01-13 14:04:26 -0500

    working for Ri3D and added stuff like outtake

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -144,16 +144,38 @@ public class Robot extends TimedRobot {
     double shooterCmd = -rightY;
 
     shooterCmd = MathUtil.applyDeadband(shooterCmd, kShooterDeadband);
-    shooterCmd = MathUtil.clamp(shooterCmd, 0.0, 1.0);
+    //shooterCmd = MathUtil.clamp(shooterCmd, 0.0, 1.0);
+    double highShooter = 0.7;
+    // if(operatorController.getSquareButton()){
+    //    highShooter = 0.1;
+    // }
+    // if(operatorController.getCircleButtonPressed()){
+    //  highShooter = 0.7;
+    // }
 
+    shooterCmd = MathUtil.clamp(shooterCmd, 0.0, highShooter);
+
+    //hold R1 to index and use right pivot (before index) to shoot
     shooterMotorLeft.set(shooterCmd);
+    //shooterMotorLeft.set(0.1);
 
-    // Operator Controller: intake while LB held
-    if (operatorController.getL1Button()) { 
+
+    // Operator Controller: intake while LB held (NO)
+    if (operatorController.getCircleButton()) { //Intake X button
       intakeMotor.set( kIntakePercentOutput);
-    } else {
+    } else if (operatorController.getSquareButton()){ //Outtake A button
+      intakeMotor.set( -0.7);
+    }
+    else 
+    {
       intakeMotor.set( 0.0);
     }
+
+    // if (operatorController.getSquareButton()) { 
+    //   intakeMotor.set( -0.7);
+    // } else {
+    //   intakeMotor.set( 0.0);
+    // }
 
     // Operator Controller: indexer while RB held 
     if (operatorController.getR1Button()) {
